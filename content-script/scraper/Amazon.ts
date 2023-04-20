2028aa9f51b07fdc47086627b7afedccfe6743ad
AughdonBreslin <augh.breslin@gmail.com>
2023-04-20 19:01:33 -0400

    Added support for product details and technical details

--- content-script/scraper/Amazon.ts
+++ content-script/scraper/Amazon.ts
@@ -1,41 +1,54 @@
 const scrapeProductDetails = () => {
-    //console.log(document);
-    const regex = new RegExp("\w*sections?_1");
-    let productDetails = document.getElementById("prodDetails") as HTMLDivElement;
+    // Product Title
+    const productTitle = document.getElementById("productTitle") as HTMLHeadingElement;
+    if (productTitle) {
+        let title = productTitle.innerText.trim();
+        if(!title) return console.log("Error: No product title found.");
+        console.log(title);
+    }
 
-    // let productDetailsString = productDetails.outerHTML
-    // let headers = productDetails.getElementsByTagName("table");
-    // console.log(headers[0]);
-    const technicalDetails = productDetails.getElementsByTagName("table")[0]
-    // const table = document.getElementById("productDetails_techSpec_section_1") as HTMLTableElement
-    for (let i = 0, row; row = technicalDetails.rows[i]; i++) {
-        // console.log(row);
-        // console.log(row.firstElementChild)
-        let tableHead = row.firstElementChild.innerText;
-        let tableData = row.lastElementChild.innerText;
-        if (tableHead == "Item Weight") {
-            console.log(tableData);
-            console.log("we gaming");
+    // Product Details
+    const productDetails = document.getElementById("detailBullets_feature_div") as HTMLDivElement;
+    if (productDetails) {
+        const unorderedList = productDetails.getElementsByTagName("ul")[0] as HTMLUListElement;
+        if(!unorderedList) return console.log("Error: No product details found.");
+
+        const listItems = unorderedList.getElementsByTagName("li");
+        if(!listItems) return console.log("Error: No elements in table found.");
+
+        for (let i = 0, listItem; listItem = listItems[i]; i++) {
+            let listItemText = listItem.innerText;
+            console.log(`${i}: ${listItemText}`);
+            if (listItemText.includes("Package Dimensions") || listItemText.includes("Product Dimensions")) { // || ...
+                let itemDimensions = listItemText.split(":")[1].split(";")[0].trim();
+                if(!itemDimensions) console.log("Warning: No item dimensions found.");
+                console.log(itemDimensions);
+
+                let itemWeight = listItemText.split(";")[1].trim();
+                if(!itemWeight) console.log("Warning: No item weight found.");
+                console.log(itemWeight);
+            }
         }
-        if (tableHead == "Base Material") {
-            console.log(tableData);
-            console.log("gamers rise up");
+    }
+
+    // Technical Details
+    const technicalDetails = document.getElementById("prodDetails") as HTMLDivElement;
+    if (technicalDetails) {
+        const table = technicalDetails.getElementsByTagName("table")[0] as HTMLTableElement;
+        if(!table) return console.log("Error: No technical details found.");
+        for (let i = 0, row; row = table.rows[i]; i++) {
+            let tableHead = row.firstElementChild.innerText.trim();
+            if(!tableHead) console.log("Warning: No table head found.");
+            console.log(`${i}: ${tableHead}`);
+            let tableData = row.lastElementChild.innerText.trim();
+            if (tableHead == "Item Weight" || tableHead == "Base Material") { // || ...
+                if(!tableData) console.log("Warning: No table data found.");
+                console.log(tableData);
+            }
         }
     }
     
-    
-        return {};
-
-    // const details = productDetails.getElementsByTagName("prod");
-    // const detailsArray = Array.from(details);
-    // const detailsObject = detailsArray.reduce((acc, detail) => {
-    //     const key = detail.getElementsByTagName("th")[0].innerText;
-    //     const value = detail.getElementsByTagName("td")[0].innerText;
-    //     acc[key] = value;
-    //     return acc;
-    // }, {});
-
-   // return detailsObject;
+    return {};
 };
 
 const Amazon = () => {
